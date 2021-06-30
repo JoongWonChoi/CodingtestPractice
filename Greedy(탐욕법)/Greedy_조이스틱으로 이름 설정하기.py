d4e9a5989aebf68cfa4d2f0ed3d58e57ec40199e
JoongWonChoi <qweasd1130@naver.com>
2021-06-30 18:08:29 +0900

    cd test : joystick problem solution

--- Greedy(탐욕법)/Greedy_조이스틱으로 이름 설정하기.py
+++ Greedy(탐욕법)/Greedy_조이스틱으로 이름 설정하기.py
@@ -11,6 +11,7 @@ def solution(name):
     ##'▶'(right) 동작을 수행하는 반복문
     while i<len(name):
         if name[i] == 'A':
+            print("A's index",i)
             if len(name) - 1 <= (i * 2) + ((len(name) - 1) - (i + 1) + 1):
             #'▶'(right)만을 수행하여 이름의 마지막 알파벳까지 도달하는것이 A를 만나 '◀'(left)을 사용하여 왼쪽 알파벳들을 탐색하는 방법보다 짧다면 그냥 오른쪽으로 쭉 이동하며 진행한다.
                 if i > 0: right += 1
@@ -26,11 +27,18 @@ def solution(name):
         i +=1
         # front리스트의 알파벳은 A부터 출발하기 때문에 B로 넘어가는 순간에 up이 추가된다. 따라서 인덱스 번호 자체가 움직인 횟수가 된다.
         # 하지만 back리스트는 Z,즉 리스트의 첫 요소부터가 A로부터 down이 1회 된 것이기 때문에 Z로 이동한 횟수인 +1을 해주어야한다.
+    #current i = index of 'A'
+    print(i)
     left += (i-1)
-   ##'◀'(left) 동작을 수행하는 반복문
-    while True:
+    #0번째 인덱스까지 left 동작!
+   ##'◀'(left) 동작을 수행하는 반복문(이름의 첫 번째 인덱스부터 시작합니다)
+    while i+1<len(name):
         left += 1
-        #A를 만나 A가 위치한 인덱스의 previous index로 넘어가기 위해 '◀'(left)를 동작한다.
+        if name[i+1] in alp_front:
+            up += alp_front.index(name[i+1])
+        elif name[i+1] in alp_back:
+            down += alp_back.index(name[i+1]) + 1
+        i += 1
 
     answer = left + right + up + down
 
@@ -61,5 +69,5 @@ def solution(name):
 
 name = "JEROEN"
 name2 = "JAN"
-print(solution(name))
+#print(solution(name))
 print(solution(name2))
